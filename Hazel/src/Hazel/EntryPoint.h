334ed218e87a07bfdcdc3a5f5d231fdd2c669fc6
Anders Strömberg <coltlaboy@yahoo.se>
2025-06-23 13:34:08 +0200

    Adds a logging system to project

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Core Logger");
+	int a = 5;
+	HZ_INFO("Initialized Client Logger");
+	HZ_INFO("Variable a = {0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
